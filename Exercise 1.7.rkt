acf19d56291cca67bce8a607a208980356f8f0b1
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-06-04 08:17:14 +0200

    Update Exercise 1.7.rkt

--- Exercise 1.7.rkt
+++ Exercise 1.7.rkt
@@ -4,23 +4,44 @@
 (define (improve guess x)
   (average guess (/ x guess)))
 
-;(define (good-enough? guess x)
-;  (< (abs (- (* guess guess) x)) 0.0001))
+--- Original vesion (of the functions) ---
 
-;(define (sqrt-iter guess x)
-;  (if (good-enough? guess x)
-;      guess
-;      (sqrt-iter (improve guess x) x)))
+;;(define (good-enough? guess x)
+;;  (< (abs (- (* guess guess) x)) 0.0001))
+
+;;(define (sqrt-iter guess x)
+;;  (if (good-enough? guess x)
+;;      guess
+;;      (sqrt-iter (improve guess x) x)))
+
+--- Alternative version (of the functions) ---
 
 (define (good-enough? guess x)
-  (< (abs (- guess (improve guess x))) 0.000001))    ; modified
+  (< (abs (- guess (improve guess x))) 0.000001))    ; the modified part
 
 (define (sqrt-iter guess x)
   (if (good-enough? guess x)
-      (improve guess x)                             ; modified
+      (improve guess x)                              ; the modified part
       (sqrt-iter (improve guess x) x)))
 
+--- Application ---
 
 (sqrt-iter 0.0202 0.004040303030)
 (sqrt-iter 5858 856752211269)
 (sqrt-iter 30420 30404040405405040)
+
+--- Results ---
+
+;; original version:
+;; 0.0642226044757862578518422...
+;; 925609.1028447159777857063666085...
+;; ?
+
+;; alternative version
+;; 0.0635633780568654171028621...
+;; 925609.1028447159777856500145315...
+;; ?
+
+--- Conclusion ---
+;; The calcuclation in the alternative version returns a more precise result compared to the original version.
+;; The very large number such as 30404040405405040 are still a problem. There is no result of the calculation in both cases even after 3 mins.
