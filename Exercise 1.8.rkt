991b8ba171d2b286707355d6a1a5cc8877e4fe74
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-06-04 09:01:19 +0200

    Create Exercise 1.8.rkt

---
+++ Exercise 1.8.rkt
@@ -0,0 +1,10 @@
+(define (improve guess x)
+(/ (+ (/ x  (* guess guess)) (* 2 guess)) 3))
+
+(define (good-enough? guess x)
+  (< (abs (- guess (improve guess x))) 0.000001))  
+
+(define (cube-root-iter guess x)
+  (if (good-enough? guess x)
+      (improve guess x)                       
+      (cube-root-iter (improve guess x) x)))
